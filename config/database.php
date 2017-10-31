fd0a6b6a9c056bbba1398cb635682f5081d758d3
Worlanyo Damalie <worlalidamalie@gmail.com>
2017-10-31 15:45:34 +0000

    Bug fix

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
             'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("CLEARDB_DATABASE_URL"))["pass"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
